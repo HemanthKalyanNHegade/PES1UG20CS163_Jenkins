1f4746142f0184c59b1a4d0c4b3cac7daecb3c21
Hemanth <116051520+HemanthKalyanNHegade@users.noreply.github.com>
2023-02-17 15:06:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World.This is Hemanth\n";
   cout << "Hello, Jenkins.My SRN is PES1UG20CS163\n";
   cout << "I have successfully built and run\n";
   return 0;
